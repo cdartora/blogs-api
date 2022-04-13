889f7383b34a93a640790d280b0e6f07efea1449
cdartora <carlosdalsoler@gmail.com>
2022-04-12 23:16:51 -0300

    feat[users]: add login and getAll controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken');
+const { User } = require('../models');
 const services = require('../services/users');
+require('dotenv').config();
+
+const secret = process.env.SECRET;
 
 const create = async (req, res) => {
   const { displayName, email, password, image } = req.body;
-  const secret = 'ayecaptain66';
   
   try {
-    const newUser = services.create(displayName, email, password, image);
-    
+    const newUser = await services.create(displayName, email, password, image);
+
     const jwtConfig = {
       expiresIn: '7d',
       algorithm: 'HS256',
@@ -21,6 +24,36 @@ const create = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await services.authenticateUser(email, password);
+    
+    const jwtConfig = {
+      expiresIn: '7d',
+      algorithm: 'HS256',
+    };
+  
+    const token = jwt.sign({ data: user }, secret, jwtConfig);
+
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(400).send({ message: 'Invalid fields' });
+  }
+};
+
+const getAll = async (req, res) => {
+  try {
+    const allUsers = await User.findAll();
+    res.status(200).send(allUsers);
+  } catch (err) {
+    res.status(500).send({ message: 'Somethin went wrong on the db.' });
+  }
+};
+
 module.exports = {
   create,
+  login,
+  getAll,
 };
