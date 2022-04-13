afb61529e79385b812465318601d15a68b8a000c
cdartora <carlosdalsoler@gmail.com>
2022-04-12 23:17:25 -0300

    feat[users]: add authenticateUser service

--- services/users.js
+++ services/users.js
@@ -2,13 +2,21 @@ const { User } = require('../models');
 
 const create = async (displayName, email, password, image) => {
   const user = await User.findOne({ where: { email } });
-  if (user) {
-    throw new Error();
-  }
+
+  if (user) throw new Error('User already exists');
+
   const { dataValues } = await User.create({ displayName, email, password, image });
+  
   return dataValues;
+};
+
+const authenticateUser = async (email, password) => {
+  const user = await User.findOne({ where: { email, password } });
+  if (!user.dataValues) throw new Error('User doesn\'t exists.');
+  return user.dataValues;
 };
 
 module.exports = {
   create,
+  authenticateUser,
 };
