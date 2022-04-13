cb069f38edc1bc73d8f91047de3046fc764151ac
cdartora <carlosdalsoler@gmail.com>
2022-04-12 23:15:43 -0300

    feat[auth]: authenticate JWT

---
+++ auth/validateJWT.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { User } = require('../models');
+
+const secret = process.env.SECRET;
+
+module.exports = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secret);
+
+    const user = await User.findOne({ where: { email: decoded.data.email } });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Erro ao procurar usuário do token.' });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
